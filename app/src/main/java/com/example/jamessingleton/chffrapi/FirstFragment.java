4b21dc022b3c46d7e73ea21c6397ac3e04679501
Frank <blandonfrank>
2016-09-10 10:08:22 -0600

    adding actual speed to graph

--- app/src/main/java/com/example/jamessingleton/chffrapi/FirstFragment.java
+++ app/src/main/java/com/example/jamessingleton/chffrapi/FirstFragment.java
@@ -1,6 +1,8 @@
 package com.example.jamessingleton.chffrapi;
 
 import android.app.Fragment;
+import android.content.Context;
+import android.content.SharedPreferences;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
 import android.view.LayoutInflater;
@@ -21,6 +23,8 @@ import com.github.mikephil.charting.listener.ChartTouchListener;
 import com.github.mikephil.charting.listener.OnChartGestureListener;
 import com.github.mikephil.charting.utils.ColorTemplate;
 
+import org.joda.time.DateTime;
+
 import java.util.ArrayList;
 import java.util.List;
 import java.util.Map;
@@ -39,11 +43,14 @@ public class FirstFragment extends Fragment implements APIRequestsUtil.APIReques
     private int itemCount = 0;
     private BarChart mChart;
     Map<String, Route> drives;
+    private SharedPreferences sharedPref;
+
 
     @Nullable
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
         myView = inflater.inflate(R.layout.first_layout, container, false);
+        sharedPref = getActivity().getPreferences(Context.MODE_PRIVATE);
 
         APIRequestsUtil.setOnAPIResponseListener(this);
         mChart = new BarChart(getActivity());
@@ -98,7 +105,7 @@ public class FirstFragment extends Fragment implements APIRequestsUtil.APIReques
                     String date = drive.getKey().toString();
                     Route route = (Route) drive.getValue();
 
-                    double value = (double) Math.round((Float.parseFloat(route.getLen()) / 1000) * 100.0) / 100.0;
+                    double value = getSpeed(route);
                     float fValue = (float) value;
                     entries.add(new BarEntry(pos++, fValue));
 
@@ -169,5 +176,19 @@ public class FirstFragment extends Fragment implements APIRequestsUtil.APIReques
     @Override
     public void onResponse(Response response) {
             populateView();
+    }
+
+    private double getSpeed(Route route) {
+
+        DateTime startTime = new DateTime(route.getStart_time());
+        DateTime endTime = new DateTime(route.getEnd_time());
+        double milliseconds = endTime.getMillis() - startTime.getMillis();
+        double distance = (double) Math.round((Float.parseFloat(route.getLen())) * 100.0) / 100.0;
+
+
+        if (sharedPref.getString("measurement", null).equals("imperial"))
+            distance = distance / 1.609344;
+
+        return distance * 3600 / milliseconds;
     }
 }
